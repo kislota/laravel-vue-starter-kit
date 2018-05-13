5cfc3da30b7edd7a06486dab926802c58d3c8eec
kislota <kislotakill@gmail.com>
2018-05-14 00:37:44 +0300

    Config JWTAuth

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\ResetsPasswords;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Password;
 
 class ResetPasswordController extends Controller
 {
@@ -36,4 +38,26 @@ class ResetPasswordController extends Controller
     {
         $this->middleware('guest');
     }
+
+	public function postReset(Request $request)
+	{
+		$this->validate($request, $this->rules(), $this->validationErrorMessages());
+
+		// Here we will attempt to reset the user's password. If it is successful we
+		// will update the password on an actual user model and persist it to the
+		// database. Otherwise we will parse the error and return the response.
+		$response = $this->broker()->reset(
+			$this->credentials($request), function ($user, $password) {
+			$this->resetPassword($user, $password);
+		}
+		);
+
+
+		if($response == Password::PASSWORD_RESET) {
+			return response()->json(['success'=>true, 'data'=>'password successfully changed']);
+		}
+		else {
+			return response()->json(['success'=>false, 'message'=>'password is not changed'], 501);
+		}
+	}
 }
---
+++ app/Models/User.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Storage;
+use Tymon\JWTAuth\Contracts\JWTSubject;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class User extends Authenticatable implements JWTSubject
+{
+	use Notifiable;
+
+	protected $fillable = [
+
+	];
+
+	protected $hidden = ['password', 'remember_token'];
+
+
+	/**
+	 * Get the identifier that will be stored in the subject claim of the JWT.
+	 *
+	 * @return mixed
+	 */
+	public function getJWTIdentifier()
+	{
+		return $this->getKey();
+	}
+
+	/**
+	 * Return a key value array, containing any custom claims to be added to the JWT.
+	 *
+	 * @return array
+	 */
+	public function getJWTCustomClaims()
+	{
+		return [];
+	}
+
+}
